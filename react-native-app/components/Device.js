ca0878536d2f5d5c05766919c447df7e18c1f474
Sreeh10 <82865021+Sreeh10@users.noreply.github.com>
2022-12-14 07:29:45 +0530

    modified dashboard

--- react-native-app/components/Device.js
+++ react-native-app/components/Device.js
@@ -5,84 +5,116 @@ import { AntDesign, FontAwesome} from '@expo/vector-icons';
 
 var Dim_width = Dimensions.get('window').width; //full width
 var Dim_height = Dimensions.get('window').height; //full width
-
 const Device = ({device, deleteDevice}) => {
-    return (
-        <View style={[styles.card, styles.elevation, device.status ? styles.blue : styles.white]}>
-            <View style={styles.cross}>
-                <Text style={styles.heading}>
-                  {device.type + " " + device.device_num}
-                </Text>
-                <TouchableOpacity onPress={() => deleteDevice(device.id)}>
-                  <View style={styles.container}>
-                    <AntDesign name="closecircle" size={25} color="maroon" />
-                  </View>
-                </TouchableOpacity>
-            </View>
-            <Text>
-                {device.status ? "ON" : "OFF"}
-            </Text>
-            {/* <TouchableOpacity >
-              <Text>Remove</Text>
-            </TouchableOpacity> */}
-            
-        </View>
-    );
-}
+  // return (
+  //     <View style={[stylesDevice.card, stylesDevice.elevation, device.status ? stylesDevice.blue : stylesDevice.white]}>
+  //         <View style={stylesDevice.cross}>
+  //             <Text style={stylesDevice.heading}>
+  //               {device.type + " " + device.device_num}
+  //             </Text>
+  //             <TouchableOpacity style = {stylesDevice.toucharea} onPress={() => deleteDevice(device.id)}>
+  //               <View style={stylesDevice.container}>
+  //                 <AntDesign name="closecircle" size={25} color="orange" />
+  //               </View>
+  //             </TouchableOpacity>
+  //         </View>
+  //         <Text>
+  //             {device.status ? "ON" : "OFF"}
+  //         </Text>
+     
+  //     </View>
+  // );
 
-// const styles = StyleSheet.create({
-//     device:{
-//         position:"relative",
-//         height:170,
-//         // width:60,
-//         margin: 10,
-//     },
-//     strong:{
-//         marginTop: 70,
-//         marginBottom: 0,
-//         marginRight:10,
-//         fontSize:18,
-//     },
-//     label:{
-//         position:"absolute",
-//         width: 0.74*Dim_width,
-//         height: 0.71*Dim_height,
-//         padding:20,
-//         borderRadius:30,
-//         shadowColor: 'black',
-//         shadowOpacity: 0.3,
-//     }
+// /*
+  return (
+
+    <View style = {[stylesDevice.card, stylesDevice.elevation, device.status ? stylesDevice.blue : stylesDevice.white]}> 
+
+      <View style = {stylesDevice.crosspart}>
+          <TouchableOpacity style = {stylesDevice.toucharea} onPress={() => deleteDevice(device.id)}>
+                <View style={stylesDevice.xmark}>
+                  <AntDesign name="closecircle" size={20} color="orange" />
+                </View>
+          </TouchableOpacity>
+      </View>
+
     
-// });
 
-const styles = StyleSheet.create({
-    heading: {
-      fontSize: 18,
-      fontWeight: '600',
-      marginBottom: 13,
-    },
-    card: {
-      borderRadius: 20,
-      paddingVertical: 45,
-      paddingHorizontal: 25,
-      width: '40%',
-      marginVertical: 10,
-    },
-    white:{
-      backgroundColor: 'white',
-    },
-    blue:{
-      backgroundColor: 'skyblue'
-    },
-    elevation: {
-      elevation: 20,
-      shadowColor: '#52006A',
-    },
-    cross:{
-      flex:1, 
-      flexDirection:'row', 
-      justifyContent:'space-between'
-    }
-  });
+      <View style = {stylesDevice.textpart} >
+      
+          <Text style={stylesDevice.heading}>
+                {device.type + " " + device.device_num}
+              </Text>
+
+          <Text>
+              {device.status ? "ON" : "OFF"}
+          </Text>
+      
+      </View>
+
+      
+    </View>
+    
+); 
+
+// */
+
+};
+
+const stylesDevice = StyleSheet.create({
+  heading: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginBottom: 13,
+  },
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    paddingBottom: 45,
+    paddingLeft: 25,
+    paddingRight: 5,
+    paddingTop: 5,
+    width: '40%',
+    marginVertical: 10,
+    borderColor : "green" ,
+    borderStyle : 'solid', 
+    borderWidth : 3 , 
+  },
+  white:{
+    backgroundColor: 'white' ,
+  },
+  blue:{
+    backgroundColor: 'rgba(180, 255, 180, 1)' ,
+  },
+
+  elevation: {
+    elevation: 20,
+    shadowColor: '#52006A',
+  },
+
+  toucharea: { 
+    // backgroundColor : 'maroon',
+  },
+
+  textpart : { 
+    // backgroundColor  : 'rgba(0,0,0,0)',
+  },
+
+  all : {
+     backgroundColor : 'white',
+  },
+
+  crosspart:{
+    // backgroundColor : 'pink',
+    // flex:1, 
+    display : 'flex',
+    flexDirection:'row', 
+    justifyContent:'flex-end',
+    // alignItems : 'right',
+  }
+  
+});
+
+
 
 export default Device
