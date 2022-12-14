00a42278f607df9cbf981c4e247663eaf6e99fdf
Sreeh10 <82865021+Sreeh10@users.noreply.github.com>
2022-12-14 07:28:23 +0530

    modified dashboard

--- react-native-app/App.js
+++ react-native-app/App.js
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, View, Image , Dimensions, ScrollView} from 'react-native';
-import React, {useState} from 'react'
+import { StatusBar , Platform,  StyleSheet, Text, View, Image , Dimensions, ScrollView ,TouchableOpacity, TouchableNativeFeedback} from 'react-native';
+import React, {useEffect, useState} from 'react'
 import Header from './components/Header';
 import Devices from './components/Devices';
+// import Device from './components/Device';
 import AddDevice from "./components/AddDevice";
 import RemainingDev from "./components/RemainingDev";
-// import Paho from 'paho-mqtt';
+import { AntDesign, FontAwesome} from '@expo/vector-icons';
 
+// import Geolocation from '@react-native-community/geolocation';
+// import { StatusBar } from 'expo-status-bar';
+// import Paho from 'paho-mqtt';
 var Dim_width = Dimensions.get('window').width; //full width
 var Dim_height = Dimensions.get('window').height; //full height
+
+
+
+
+
+
 
 const App = () => {
   const [curr_added, setCurrDevices] = useState([1,4,3,6,7])
@@ -89,6 +99,7 @@ const App = () => {
     })
   }
 
+
   // const clientId = "OhkTEh00FAEUIA4oNRstJis"
   // const host = '//mqtt3.thingspeak.com/mqtt'
 
@@ -129,29 +140,111 @@ const App = () => {
   // })
 
   // console.log("connected1")
-
-
+// style = {show_remaining ? styles.gridExpand : styles.gridCollapse} } rgb(255,100,180)
   return (
-    <ScrollView style={styles.container}>
-      <Header />
-      <View>
-        {connected_dev.length > 0 ? <Devices devices={connected_dev} curr_added={curr_added} deleteDevice={deleteDevice}/> : " No Connected Device"}
-      </View>
-      <View>
-        {connected_dev.length < 7 ? <AddDevice toggleRemaining={toggleRemaining}/> : ""}
-      </View>
-      <View>
-        {show_remaining ? (remaining_dev.map((not_connected) => (<RemainingDev key={not_connected.id} remaining={not_connected} AddDeviceToConnected={AddDeviceToConnected}/>))) : ""}
-      </View>
-    </ScrollView>
+    <View style = {styles.total}>
+        <Header />
+
+        <Text style = { {color : "rgb(2,20,20)" , paddingTop : 5 , fontWeight : '600',  alignSelf : "center", borderBottomColor : "rgba(200,204,208,0.7)",borderTopColor : "rgba(200,204,208,0.9)", borderBottomWidth : 0 , }}>  {connected_dev.length > 0 ? "Connected (" + connected_dev.length + ")" : "No Device Connected"} </Text>
+
+        <ScrollView style = {show_remaining ? styles.gridExpand : styles.gridCollapse}  >
+
+            {connected_dev.length > 0 ? <Devices devices={connected_dev} curr_added={curr_added} deleteDevice={deleteDevice}/> : <Text style = { styles.noDeviceText}> </Text>}
+  
+          
+        </ScrollView>
+
+        
+        <View style = {show_remaining ? styles.addButtonBaseExpand : styles.addButtonBaseCollapse} >
+          <AddDevice toggleRemaining={toggleRemaining} state = {!show_remaining} />
+        </View>
+
+        <ScrollView style = {show_remaining ? styles.additionListExpand : styles.additionListCollapse } >
+          
+    
+          <View>
+            { !show_remaining ? ""
+                  : (connected_dev.length == devices.length ) ? <Text style = { styles.noDeviceText}> Nothing to Add</Text>
+                            
+                      :(remaining_dev.map((not_connected) => (<RemainingDev key={not_connected.id} remaining={not_connected} AddDeviceToConnected={AddDeviceToConnected}/>))) 
+                          
+                          
+            }
+          </View>
+
+          
+
+        </ScrollView>
+    </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor: "aliceblue",
+  gridExpand:{
+    marginTop : 10 ,
+    paddingTop : 20 , 
+    backgroundColor: "rgba(0,10,10,0.7)",
     width:Dim_width,
-    height:Dim_height+50,
+    height:'30%',
+
+  },
+  
+  addButtonBaseExpand:{
+    justifyContent: 'space-around',
+    height : '10%',
+    // backgroundColor: 'blue',
+  },
+
+  additionListExpand : {
+    // backgroundColor: "cyan",
+    backgroundColor: "rgb(0,50,50)",
+
+    width:Dim_width,
+    height:'40%',
+
+  },
+
+
+  gridCollapse:{
+    marginTop : 10 ,
+    // paddingTop : 20 , 
+    // paddingBottom : 40 , 
+    backgroundColor: "rgba(0,10,10,0.7)",
+
+    // backgroundColor: "aliceblue",
+    width:Dim_width,
+    height:600,
+  },
+
+  addButtonBaseCollapse:{
+    justifyContent: 'space-around',
+    paddingVertical : 0 , 
+    height : '10%',
+    // backgroundColor: 'blue',
+  },
+
+  additionListCollapse : {
+    // backgroundColor: "cyan",
+    backgroundColor: "rgb(0,50,50)",
+    width:Dim_width,
+    height:'0%',
+
+  },
+
+
+  noDeviceText : { 
+    textAlign : 'center' ,
+    color : "white" ,
+
+  } , 
+  total:{
+    display : 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    height:'100%',
+    backgroundColor  : 'rgba(10,200,220,1)',
+
   }
 });
 
