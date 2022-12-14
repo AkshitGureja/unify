9a90fdea003db483d0cb38b81b0144ebd5dcc5eb
Sreeh10 <82865021+Sreeh10@users.noreply.github.com>
2022-12-14 07:31:00 +0530

    modified dashboard

--- react-native-app/components/Header.js
+++ react-native-app/components/Header.js
@@ -1,68 +1,116 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import React from 'react'
+import { StatusBar , StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect , useState   } from 'react'
+// import Geolocation from '@react-native-community/geolocation';
 
 const Header = () => {
-  return (
-    <View style={[styles.card, styles.elevation]}>
-        <View style={{flexDirection:'row'}}> 
-            <Image source={require('./../assets/unify.png')} /> 
-            <Text style={styles.heading}>
-              <Text>
-                Cloudy
-              </Text>
-              <Text>
-                10 Dec 2022
-              </Text>
-            </Text>  
-        </View>
-        
-        <Text>  
-            Heading 2
-        </Text>
-    </View>
-    // <View>
-    //   <View style={{flexDirection:'row'}}>
-    //     <Image source={require('./../assets/unify.png')} />
-    //     <View>
-    //       <View>
-    //         <Text>Cloudy</Text>
-    //       </View>
-    //       <Text>10 Dec 2022</Text>
-    //     </View>
-    //   </View>
-    //   <View style={{flexDirection:'row'}}>
-    //     <View>
-    //       <Text>26°C</Text>
-    //       <Text>Temperature</Text>
-    //     </View>
-    //     <View>
-    //       <Text>48.2%</Text>
-    //       <Text>Outdoor Humidity</Text>
-    //     </View>
-    //   </View>
-    //     {/* <Text  style={styles.text} >Hello World</Text> */}
-    // </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  heading: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginBottom: 13,
-  },
-  card: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    paddingVertical: 45,
-    paddingHorizontal: 25,
-    width: '100%',
-    marginVertical: 10,
-  },
-  elevation: {
-    elevation: 20,
-    shadowColor: '#52006A',
-  },
-});
+  // Banglore by default
+
+  const [latitude, setLatitude] = useState(13.23) ;
+  const [longitude, setLongitude] = useState(78.29) ;
+
+  
+  
+  let WeatherFromLoc_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=fd8451f6faf82427d680c1f9b37f7b16" ; 
+  
+  
+  const [temperature, setTemperature] = useState(30);
+  const [description, setDescription] = useState('mist');
+  
+  useEffect(() => {
+    fetch ( `${WeatherFromLoc_URL}&lat=${latitude}&lon=${longitude}`)
+    .then ( (x) => x.json() )
+    .then ( (x) => { 
+      setDescription( x["weather"][0]["description"] ) ;
+      setTemperature( x["main"]["temp"] );
+    } ) ;
+    
+
+    setLatitude ( 19) ;
+    setLongitude ( 73) ;
+
+
+  } , [])
+
+
+
+  const date = new Date().toString();
+
+  StatusBar.setHidden(false, 'none');
+  // StatusBar.setBackgroundColor('blue');
+  StatusBar.setBackgroundColor('rgba(0,128,128,0.5)');
+  // StatusBar.setTranslucent(true);
+
+//  
+  return (
+
+      <View style = {styles0.overall}> 
+
+          <View style = {styles0.logobase }>
+          <Image source = {require("../assets/unify.png")} style = {styles0.logo}/>
+
+          {/* <Text style = { styles0.textpart} > Unify </Text> */}
+
+          </View>
+          
+          <View style = { {width : '40%' , height : '100%' , display : 'flex' , flexDirection : 'column' , justifyContent : 'space-evenly'}}> 
+                  
+                  <Text> {date.slice(0,3)}, {date.slice(4,10)}  </Text>
+                  <Text> {temperature} °C </Text>
+                  <Text> {description} </Text>
+          </View>
+
+
+      </View>
+
+    );
+
+  }
+
+  const styles0 = StyleSheet.create({
+
+    overall : { 
+      display : 'flex' ,
+      flexDirection : 'row' , 
+      justifyContent : 'space-around',
+      alignItems : 'center',
+      height : '20%', 
+      width : '100%',
+      paddingVertical : '2%' ,
+      marginTop : StatusBar.currentHeight , 
+      marginBottom: '0%' , 
+      borderBottomColor : 'black' ,
+      borderBottomWidth : 1 ,
+      backgroundColor : '#9fff9f' ,
+    },
+    textpart : { 
+
+      alignSelf : 'center',
+      
+    
+   },
+   logobase : {
+    justifyContent : 'center',
+    alignItems : 'center',
+    // border : '1% solid yellow' ,
+    // backgroundColor : 'white' ,
+    // borderWidth : 1 , 
+    // borderColor : 'yellow' ,
+    height : '75%',
+    width : '25%',
+    // 
+
+
+   },
+
+   logo : { 
+    height : '100%' ,
+    width : '100%' ,
+    resizeMode : 'center' ,
+   }
+
+  }
+  );
+
 
 export default Header
